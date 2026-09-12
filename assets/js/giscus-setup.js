412c00e29271c116443c1afa9b5092ef97dc10e4
sangzikim <sanghoisworking@gmail.com>
2026-09-12 11:13:12 +0000

    Deploying to gh-pages from @ sangzikim/sangzikim.github.io@2a3407298615e61b04ddffa415dbfd5003aae735 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SangHo-Daniel-Kim/SangHo-Daniel-Kim.github.io",
+    "data-repo": "sangzikim/sangzikim.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
